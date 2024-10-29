f39c1b5015a2e71020888e50411ef6c4adb88800
mohammad <mohammadfallah.w@gmail.com>
2024-10-29 22:25:28 +0330

    feat: :sparkles: Showing a skeleton while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,25 +1,33 @@
 import useGenres from "@/hooks/use-genres";
 import getImage from "@/utilities/get-image";
 import { HStack, Image, Link, List } from "@chakra-ui/react";
+import { SkeletonText } from "./ui/skeleton";
 
 const GenreList = () => {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
     <List.Root variant="plain">
-      {genres.map((genre) => (
-        <List.Item key={genre.id} pb={3}>
-          <HStack>
-            <Image
-              src={getImage(genre.image_background)}
-              boxSize="32px"
-              objectFit="cover"
-              borderRadius={8}
-            />
-            <Link fontSize="lg">{genre.name}</Link>
-          </HStack>
-        </List.Item>
-      ))}
+      {isLoading
+        ? skeletons.map((skeleton) => (
+            <List.Item key={skeleton} pb={3}>
+              <SkeletonText />
+            </List.Item>
+          ))
+        : genres.map((genre) => (
+            <List.Item key={genre.id} pb={3}>
+              <HStack>
+                <Image
+                  src={getImage(genre.image_background)}
+                  boxSize="32px"
+                  objectFit="cover"
+                  borderRadius={8}
+                />
+                <Link fontSize="lg">{genre.name}</Link>
+              </HStack>
+            </List.Item>
+          ))}
     </List.Root>
   );
 };
