04e223666b53e88e65c2db2a07c40d0eadbb702f
mohammad <mohammadfallah.w@gmail.com>
2024-10-29 14:37:37 +0330

    feat: :sparkles: Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,16 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Grid gridColumn={12}>
+    <Grid gridColumn={12} px={4}>
       <GridItem colSpan={12}>
         <Navbar />
       </GridItem>
 
-      <GridItem
-        display={{ base: "none", lg: "block" }}
-        colSpan={{ lg: 2 }}
-        bg="gold"
-      >
+      <GridItem display={{ base: "none", lg: "block" }} colSpan={{ lg: 2 }}>
         Sidebar
       </GridItem>
 
-      <GridItem colSpan={{ base: 12, lg: 10 }} bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem colSpan={{ base: 12, lg: 10 }}>Main</GridItem>
     </Grid>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between">
       <Image src={logo} boxSize="60px" />
-      <Text>GameHub</Text>
+      <ColorModeButton />
     </HStack>
   );
 };
