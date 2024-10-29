d4bc7ffb75a1807d14191656ca4417058f255a50
mohammad <mohammadfallah.w@gmail.com>
2024-10-29 22:16:40 +0330

    feat: :sparkles: Displaying the genres

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import GenreList from "./components/GenreList";
 
 const App = () => {
   return (
-    <Grid gridColumn={12} gap={5} px={4}>
-      <GridItem colSpan={12}>
+    <Grid gridColumn={2} gridTemplateColumns={"1fr 5fr"} gap={5} px={4}>
+      <GridItem colSpan={2}>
         <Navbar />
       </GridItem>
 
-      <GridItem display={{ base: "none", lg: "block" }} colSpan={{ lg: 2 }}>
+      <GridItem display={{ base: "none", lg: "block" }} colSpan={{ lg: 1 }}>
         <GenreList />
       </GridItem>
 
-      <GridItem colSpan={{ base: 12, lg: 10 }}>
+      <GridItem colSpan={{ base: 2, lg: 1 }}>
         <GameGrid />
       </GridItem>
     </Grid>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,26 @@
 import useGenres from "@/hooks/use-genres";
+import getImage from "@/utilities/get-image";
+import { HStack, Image, Link, List } from "@chakra-ui/react";
 
 const GenreList = () => {
   const { data: genres } = useGenres();
 
   return (
-    <ul>
+    <List.Root variant="plain">
       {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <List.Item key={genre.id} pb={3}>
+          <HStack>
+            <Image
+              src={getImage(genre.image_background)}
+              boxSize="32px"
+              objectFit="cover"
+              borderRadius={8}
+            />
+            <Link fontSize="lg">{genre.name}</Link>
+          </HStack>
+        </List.Item>
       ))}
-    </ul>
+    </List.Root>
   );
 };
 
