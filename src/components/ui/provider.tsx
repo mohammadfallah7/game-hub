0d04ca3a2f36f274f510112497ccab3fef48bc0e
mohammad <mohammadfallah.w@gmail.com>
2024-10-31 11:44:50 +0330

    feat: :sparkles: Customize theme

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,12 +1,13 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import { ColorModeProvider } from "./color-mode"
+import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeProvider } from "./color-mode";
+import { system } from "../../theme";
 
 export function Provider(props: React.PropsWithChildren) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <ColorModeProvider>{props.children}</ColorModeProvider>
     </ChakraProvider>
-  )
+  );
 }
