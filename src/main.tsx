ba77f718e2b3b8d4b91b825ad92731d4b00a3aca
mohammad <mohammadfallah.w@gmail.com>
2024-10-31 14:18:22 +0330

    feat: :sparkles: Install react query and added it to the project

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
