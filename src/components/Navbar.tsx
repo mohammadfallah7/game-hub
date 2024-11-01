d41506a92bc4add83217c29688b86426d2d36cae
mohammad <mohammadfallah.w@gmail.com>
2024-11-01 17:11:04 +0330

    fix: :bug: Fix the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import SearchInput from "./SearchInput";
 import { ColorModeButton } from "./ui/color-mode";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
     <HStack mb={5}>
-      <Image src={logo} boxSize="60px" />
+      <Link to="/">
+        <Box width="60px">
+          <Image objectFit="cover" src={logo} />
+        </Box>
+      </Link>
       <SearchInput />
       <ColorModeButton />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,18 +2,23 @@ import useGameQuery from "@/stores/use-game-query";
 import { Input } from "@chakra-ui/react";
 import { useRef } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import { InputGroup } from "./ui/input-group";
 
 const SearchInput: React.FC = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearch = useGameQuery((state) => state.setSearch);
+  const navigate = useNavigate();
 
   return (
     <form
       style={{ width: "100%" }}
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current && ref.current.value) setSearch(ref.current.value);
+        if (ref.current) {
+          setSearch(ref.current.value);
+          navigate("/");
+        }
       }}
     >
       <InputGroup startElement={<FaSearch />} width="full">
